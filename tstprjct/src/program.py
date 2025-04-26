481ac3ba73a2330ce5cdd6d1f890dd0d4740fc3d
Stan Dev <stanislav.kad@gmail.com>
2025-04-26 13:33:03 +0300

    New section: PyCherm and new project starting

---
+++ tstprjct/src/program.py
@@ -0,0 +1,61 @@
+#type hints
+import random
+from typing import Optional, Any, List, Iterable, Generator
+
+
+# class Character:
+#     def __init__(self, armor, power):
+#         self.power = power
+#         self.armor = armor
+#         self.health = 100
+#
+#     def hit(self, damage):
+#         self.health -= damage
+#
+#     def is_alive(self):
+#         return self.health > 0
+#
+#
+# c1 = Character('lol', 'kek')
+
+#here we expect int values as the params, but we haven't any type protection
+
+class Character:
+    def __init__(self, armor: int, power: int):
+        self.power = power
+        self.armor = armor
+        self.health = 100
+
+    def hit(self, damage):
+        self.health -= damage
+
+    def is_alive(self):
+        return self.health > 0
+
+#c1 = Character('lol', 'kek') here linter will show us typeerrors
+c1 = Character(1, 1) # now nothing bad
+
+#def vars with types
+
+amount: int
+amount = None # will how an error
+
+price: Optional[int]
+
+price = None # now we can use it with None:)
+
+attack: Any  # any type
+
+quotes: List[str] = []
+quotes.append("lol")
+quotes.append(1) # typechecker will show an error
+
+
+#foo typing
+def rnd_stream(min_val: int, max_val: int) -> Generator[int]:
+    while True:
+        yield random.randint(min_val, max_val)
+
+lol = rnd_stream(1, 100)
+
+print(next(lol))
